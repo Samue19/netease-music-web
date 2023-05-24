97f411ce5e460ea2e847331df913155e2982e9dc
Samuel <2769461869@qq.com>
2023-05-24 23:22:07 +0800

    首页基本布局

---
+++ src/components/header/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="head">
+        <!-- logo -->
+        <div>
+            网易云音乐
+        </div>
+        <!-- 搜索 -->
+        <div>
+            search
+        </div>
+        <!-- 登录状态 -->
+        <div>
+            login
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.head {
+    width: 100%;
+    height: 80px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    background: rgb(255, 43, 45);
+}
+</style>
